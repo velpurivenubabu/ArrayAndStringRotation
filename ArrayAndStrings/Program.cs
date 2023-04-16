5a9e89218c4a8733042585827d876f6a3e9ca581
venubabu <velpurivenubabu123@gmail.com>
2023-04-16 22:53:36 +0530

    [Venubabu] Arrys

---
+++ ArrayAndStrings/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace ArrayAndStringRotation
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] names = new string[4] { "Venu", "Vikki", "Varun", "Vamsi" };
+            //Accrssing Elements in Array
+            foreach (string name in names)//Using Foreachloop
+            {
+                Console.WriteLine(name);
+            }
+            Console.WriteLine("- - - - - - - ");
+            for (int i = 0; i < names.Length; i++)//Using Forloop
+            {
+                Console.WriteLine(names[i]);
+            }
+            Console.WriteLine("- - - - -  -");
+            Console.WriteLine("Enter String");
+            string input = Console.ReadLine();// String reversing
+            char[] arr = { ' ', ',', '\\', '/' };
+            string[] words = input.Split(arr);
+            string reverse = "";
+            foreach (string word in words)
+            {
+                string result = " ";
+                for (int i = 0; i <= (word.Length - 1); i++)
+                {
+                    result = word[i] + result;
+                }
+                Console.WriteLine(result);
+                reverse = result + reverse;
+            }
+            Console.WriteLine(reverse);
+        }
+    }
+
+
+
+}
